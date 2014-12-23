2343e5f35675696e1aa4b6acdeb37990670d20b4
tohru_yamamoto <gw0apa00@gmail.com>
2014-12-23 19:02:13 +0900

    books Recests_spec追加

---
+++ spec/factories/books.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :book do
+  	title      Faker::Lorem.word
+  	author     Faker::Lorem.word
+  	publisher  Faker::Lorem.word
+  	issue_date Time.now.strftime('%Y-%m-%d')
+  	status     Faker::Number.digit
+  end
+end
