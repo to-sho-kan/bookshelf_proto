307efc09bc0e75403e10857f39e3838f897833ab
tohru_yamamoto <gw0apa00@gmail.com>
2014-12-29 14:31:10 +0900

    [#85069402]書籍貸出API実装

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141221090245) do
+ActiveRecord::Schema.define(version: 20141229051528) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20141221090245) do
     t.string   "publisher"
     t.date     "issue_date"
     t.integer  "status"
+  end
+
+  create_table "user_books", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "book_id"
+    t.date     "loan_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
