03b0c5253ddf52e3a3924fe9df841e8ac6ffcdda
tohru_yamamoto <gw0apa00@gmail.com>
2015-01-01 17:41:49 +0900

    [#85207862] スネークケース変換追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,11 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :memo)
+    snake_params.require(:user).permit(:first_name, :last_name, :memo)
+  end
+
+  def snake_params
+    params['user'] = Hash[params['user'].map { |k, v| [k.underscore, v] }]
+    params
   end
 end
